d927412ea3c9afffab40b324b07884254c2ddfe3
nguyenvu2801 <86769475+nguyenvu2801@users.noreply.github.com>
2025-10-14 12:12:53 +0700

    dotween

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,7 +8,7 @@ public class UIManager : GameSingleton<UIManager>
 {
     [SerializeField] private Image timerBar;
     [SerializeField] private Image screenEdgeWarning;
-
+    private Tween scaleTween;
     void Start()
     {
         TimerManager.Instance.OnTimerChanged += UpdateUI;
@@ -19,6 +20,15 @@ public class UIManager : GameSingleton<UIManager>
         timerBar.fillAmount = percent;
         screenEdgeWarning.color = new Color(1, 0, 0, Mathf.Lerp(0f, 1f, 1 - percent));
 
+        // Scale effect: from 2x when timer starts to 1x when timer ends
+        scaleTween?.Kill();
+
+        // Calculate target scale (2 -> 1 as timer decreases)
+        float targetScale = Mathf.Lerp(2f, 1f, 1 - percent);
+
+        // Tween the scale smoothly
+        scaleTween = screenEdgeWarning.rectTransform.DOScale(targetScale, 0.25f)
+            .SetEase(Ease.OutQuad);
         //if (percent < 0.2f)
         //    AudioManager.Instance.PlayHeartbeatFast();
         //else
