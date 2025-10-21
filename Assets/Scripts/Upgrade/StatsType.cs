96c550f8aed17f4783b644ceaffc7272215ccfbd
nguyenvu2801 <86769475+nguyenvu2801@users.noreply.github.com>
2025-10-21 10:00:57 +0700

    a

---
+++ Assets/Scripts/Upgrade/StatsType.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum StatsType 
+{
+    MaxTimer,
+    TimerDecayRate,
+    KillRechargeAmount,
+    ComboBonusDuration,
+    ComboPower,
+    DashRange,
+    DashCooldown,
+    DashDamage,
+    DashRecovery,
+    CriticalDashChance,
+    EssenceGain,
+    DropMagnetRadius,
+    LastBreath // etc.
+}
